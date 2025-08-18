399db695ee676c8cb0bddf3e9bd1f988683e9eeb
Nathan Orang'o <nathanorango0@gmail.com>
2025-08-18 18:11:49 +0300

    Register CustomUser

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,24 +1,28 @@
-# advanced_features_and_security/LibraryProject/bookshelf/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Book
 
-@admin.register(CustomUser)
+
 class CustomUserAdmin(UserAdmin):
-    # show extra profile fields on the user change page
+    # show extra fields on the change form
     fieldsets = UserAdmin.fieldsets + (
         ("Profile", {"fields": ("date_of_birth", "profile_photo")}),
     )
-    # show extra profile fields on the user add page
+    # show extra fields on the add form
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Profile", {"fields": ("date_of_birth", "profile_photo")}),
     )
+    # columns in the changelist
     list_display = ("username", "email", "is_staff", "date_of_birth")
-    search_fields = ("username", "email")
+
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display  = ("id", "title", "author", "publication_year")
+    list_display = ("id", "title", "author", "publication_year")
     search_fields = ("title", "author")
-    list_filter   = ("publication_year",)
-    ordering      = ("title",)
+    list_filter = ("publication_year",)
+    ordering = ("title",)
